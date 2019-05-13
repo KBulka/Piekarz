5e4ab720f91682ee741e603751652e2f1ea7ad1f
Bulka <odedyrosyt@gmail.com>
2019-05-13 11:45:06 +0200

    Piekarz_Start

---
+++ Piekarz/Assets/Scripts/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Ball : MonoBehaviour
+{
+
+    [SerializeField] PaddleMovement paddle1;
+    [SerializeField] float xPush;
+    [SerializeField] float yPush;
+    [SerializeField] bool hasStarted = false;
+    [SerializeField] Rigidbody2D rb;
+    Vector2 paddleToBallVector;
+    private void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        rb = GetComponent<Rigidbody2D>();
+        rb.simulated = false;
+    }
+    private void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            rb.simulated = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
